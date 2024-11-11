388534a422fd3a764a67a62afc6ec99fd6f23cae
rabirabii <wahyubudiman0624@gmail.com>
2024-11-11 17:10:15 +0700

    Just update some

--- E-Commerce.Data/obj/Debug/net8.0/E-Commerce.Data.AssemblyInfo.cs
+++ E-Commerce.Data/obj/Debug/net8.0/E-Commerce.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337f505c012c677dbedb8496eff68d7ae2ba6df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
